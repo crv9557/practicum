ae9a59c6eb35bc8e1f18983c6c4abdb3cc9ab224
Connor Ring <connorring7778@sjccs142.saintjoe.edu>
2015-03-30 15:56:07 -0400

    Finished modification of match.js

--- match.js
+++ match.js
@@ -1,5 +1,5 @@
-var input = "A string with 3 numbers in it... 42 and 88.";
-var number = /\b(\d+)\b/g;
+var input = "A string with 7 numbers in it... 42 and 88, 123 and 16, 4 and 22.";
+var number = /\b(\d{2})\b/g;
 var match;
 while (match = number.exec(input))
   console.log("Found", match[1], "at", match.index);
