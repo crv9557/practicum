56fbfb146e493f367def8c48378c042aaec42c6a
Connor Ring <connorring7778@sjccs142.saintjoe.edu>
2015-03-30 15:49:57 -0400

    Finished the modified counter program

--- counter.js
+++ counter.js
@@ -1,10 +1,12 @@
-var stock = "1 lemon, 2 cabbages, and 101 eggs";
+var stock = "1 lemon, 2 cabbages, and 102 eggs";
 function minusOne(match, amount, unit) {
   amount = Number(amount) - 1;
   if (amount == 1) // only one left, remove the 's'
     unit = unit.slice(0, unit.length - 1);
   else if (amount == 0)
     amount = "no";
+  else if (amount > 100)
+    amount = "zillions of";
   return amount + " " + unit;
 }
 print(stock.replace(/(\d+) (\w+)/g, minusOne));
